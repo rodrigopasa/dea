2a376fa870b99107a9d8316178b7d5b06ad91f04
Hisoka <rprodrigopasa@gmail.com>
2025-07-17 14:59:53 -0300

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,6 +1,11 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+// Esta é a forma correta de obter __dirname em Módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
@@ -42,7 +47,11 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.use("*", (req, res, next) => {
+    // Ignore API calls
+    if (req.originalUrl.startsWith('/api')) {
+        return next();
+    }
     const indexPath = path.resolve(distPath, "index.html");
     if (fs.existsSync(indexPath)) {
       res.sendFile(indexPath);
